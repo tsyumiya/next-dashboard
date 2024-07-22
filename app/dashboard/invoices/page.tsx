a4235c4eb1dc11821d4eb4bbda6fca56eaec7366
yaskadju <thales.y@gmail.com>
2024-07-22 19:38:44 -0300

    pagination and search

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,10 +5,12 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons"
 import { lusitana } from "@/app/ui/fonts"
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
 import { Suspense } from "react"
+import { fetchInvoicesPages } from "@/app/lib/data"
 
 export default async function Page({ searchParams }: { searchParams?: { query?: string; page?: string } }) {
   const query = searchParams?.query || ""
   const currentPage = Number(searchParams?.page) || 1
+  const totalPages = await fetchInvoicesPages(query)
 
   return (
     <div className="w-full">
@@ -22,7 +24,9 @@ export default async function Page({ searchParams }: { searchParams?: { query?: 
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
-      <div className="mt-5 flex w-full justify-center">{/* <Pagination totalPages={totalPages} /> */}</div>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
     </div>
   )
 }
