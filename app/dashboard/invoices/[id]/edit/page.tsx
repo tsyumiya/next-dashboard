bef476753733c236555e6156066c0e502f054d62
yaskadju <thales.y@gmail.com>
2024-07-25 17:51:00 -0300

    handling errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,10 +1,17 @@
 import Form from "@/app/ui/invoices/edit-form"
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs"
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data"
+import { notFound } from "next/navigation"
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id
   const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()])
+
+  console.log("teste")
+  if (!invoice) {
+    console.log("entrou em notFound")
+    notFound()
+  }
 
   console.log(customers)
   return (
